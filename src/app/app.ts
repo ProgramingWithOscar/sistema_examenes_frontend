029db899833802ffed733b6a5f5198ef00cc54ef
Oscar Poveda <oscarpoveda679@gmail.com>
2025-07-17 22:33:42 -0500

    Primer commit -> avances en el diseño del signup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatButtonModule } from '@angular/material/button';
+
+//compontes creados
+import { Navbar } from './components/navbar/navbar';
+import { Signup } from './pages/signup/signup';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    Navbar,
+    Signup
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
